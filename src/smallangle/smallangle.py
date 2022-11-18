470d44ae639fb62c288d22debae323fc138925f0
Coen Bartels <coen.bartels@student.uva.nl>
2022-11-18 14:31:42 +0100

    6.12
    sin function callable on command
    tan function callable on command
    removed sin(10) request
    made into an application

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,19 +4,54 @@ from numpy import pi
 import pandas as pd
 
 
+
+
+
+
+
+# def sin(number):
+#     x = np.linspace(0, 2 * pi, number)
+#     df = pd.DataFrame({"x": x, "{function} (x)": np.function(x)})
+#     print(df)
+#     return
+
+
+# def tan(number):
+#     x = np.linspace(0, 2 * pi, number)
+#     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
+#     print(df)
+#     return
+
+
+@click.group()
+def cmd_group():
+    pass
+
+
+
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 1,
+)
+
+
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 1,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
+
     return
-
-
-if __name__ == "__main__":
-    sin(10)
