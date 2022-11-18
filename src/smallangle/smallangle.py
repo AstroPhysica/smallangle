04e2f2babd2a41f73c7946728b4c8b43ec44ca8e
Coen Bartels <coen.bartels@student.uva.nl>
2022-11-18 15:03:09 +0100

    finishing script
    unnecessary comments have been removed.
    Docstrings for both the sin and tan function have been added
    added comments for each element which lacked information.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,31 +4,14 @@ from numpy import pi
 import pandas as pd
 
 
-
-
-
-
-
-# def sin(number):
-#     x = np.linspace(0, 2 * pi, number)
-#     df = pd.DataFrame({"x": x, "{function} (x)": np.function(x)})
-#     print(df)
-#     return
-
-
-# def tan(number):
-#     x = np.linspace(0, 2 * pi, number)
-#     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
-#     print(df)
-#     return
-
-
+#create the group for subcommando's
 @click.group()
 def cmd_group():
     pass
 
 
-
+#first subcomando: sin function
+#will run the function: sin(number) on command run_smallangle sin -n <number>
 @cmd_group.command()
 @click.option(
     "-n",
@@ -36,20 +19,39 @@ def cmd_group():
     default = 1,
 )
 
-
+#function sin(number) used in the first subcommand
 def sin(number):
+    """run through the sin of 0,2pi with number amount of steps
+    Makes data frame out of the x and sin(x) values
+    Prints dataframe
+
+    Args:
+        number (INTEGER): integer value, determines amount of steps
+    """   
+  
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
+#second subcommando: tan function
+#will run the function: tan(number) on command run_smallangle tan -n <number>
 @cmd_group.command()
 @click.option(
     "-n",
     "--number",
     default = 1,
 )
+
+#function tan(number) used in the second subcommand
 def tan(number):
+    """run through the tan of 0,pi with number amount of steps
+    Makes data frame out of the x and sin(x) values
+    Prints dataframe
+
+    Args:
+        number (INTEGER): integer value, determines amount of steps
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
